d55084b5bf99c5a279868fc957822cf00b0e5104
Freddy Caione <fac299@nyu.edu>
2023-02-15 16:58:08 -0500

    post container styling

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -12,18 +12,18 @@ const Posts = () => {
 	let { id } = useParams()
 
 	let navigate = useNavigate()
-	
+
 	const getPosts = async () => {
 		const response = await axios.get(
 			`http://localhost:3001/api/books/${id}/posts`
-			)
-			setPosts(response.data)
+		)
+		setPosts(response.data)
 	}
-	
+
 	useEffect(() => {
 		getPosts()
 	}, [])
-	
+
 	const handleDelete = async (postId) => {
 		const response = await swal({
 			title: "Are you sure you want to delete this post",
@@ -43,42 +43,56 @@ const Posts = () => {
 	}
 
 	const postsComponent = posts.map((post) => (
+		<>
 			<div className="post-container" key={post._id}>
 				<div className="post--user">
-					<p>by: {post.user ? post.user : "anonymous"}</p>
+					<p className="username">
+						by: <span className="italic">{post.user ? post.user : "anonymous"}</span>
+					</p>
 				</div>
 
 				<div className="post--content">
 					<h3>{post.title}</h3>
 					<p>{post.content}</p>
 				</div>
-				
+
 				{localStorage.getItem("userId") === post.user && (
 					<div className="post--buttons">
-						<button 
-						type="button" 
-						onClick={() => handleUpdate(post._id)}
-						className="button--icon"
+						<button
+							type="button"
+							onClick={() => handleUpdate(post._id)}
+							className="button--icon"
 						>
-						<img src={update} alt="trash-icon" className="trash-icon"/>
+							<img
+								src={update}
+								alt="trash-icon"
+								className="trash-icon"
+							/>
 						</button>
 
-						<button 
-						type="button" 
-						onClick={() => handleDelete(post._id)}
-						className="button--icon"
+						<button
+							type="button"
+							onClick={() => handleDelete(post._id)}
+							className="button--icon"
 						>
-						<img src={trashIcon} alt="trash-icon" className="trash-icon"/>
+							<img
+								src={trashIcon}
+								alt="trash-icon"
+								className="trash-icon"
+							/>
 						</button>
-					</div>	
+					</div>
 				)}
 			</div>
+			<hr />
+		</>
 	))
 
 	return (
 		<>
+			<h2>Reviews!</h2>
 			{postsComponent}
-        </>
+		</>
 	)
 }
 
