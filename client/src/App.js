afa0445aceb73200eef62dfdcb42e450e36f4191
Freddy Caione <fac299@nyu.edu>
2023-02-16 14:52:03 -0500

    scrollToTop compononet

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import BooksForm from "./components/BooksForm"
 import Login from "./pages/Login"
 import PostsUpdateForm from "./components/PostsUpdateForm"
 import Favorites from "./pages/Favorites"
+import ScrollToTop from "./components/ScrollToTop"
 
 function App() {
 	const [user, setUser] = useState(localStorage.getItem("userId"))
@@ -20,37 +21,45 @@ function App() {
 
 	return (
 		<div className="App">
-			{user && <NavBar 
-			setUser={setUser}
-			/>}
-			<Routes>
-				<Route
-					path="/"
-					element={
-						user ? (
-							<Books />
-						) : (
-							<Login
-								user={user}
-								setUser={setUser}
-								successMessage={successMessage}
-							/>
-						)
-					}
-				/>
-				<Route
-					path="/signup"
-					element={<Signup setSuccessMessage={setSuccessMessage} />}
-				/>
-				<Route path="/books/add" element={<BooksForm />} />
-				<Route
-					path="/books/:id"
-					element={<BookDetails user={user} />}
-				/>
-				<Route path="/books/:id/new" element={<PostsForm />} />
-				<Route path="/posts/:id/update" element={<PostsUpdateForm />} />
-				<Route path="/favorites" element={<Favorites user={user} />} />
-			</Routes>
+			<ScrollToTop>
+				{user && <NavBar setUser={setUser} />}
+				<Routes>
+					<Route
+						path="/"
+						element={
+							user ? (
+								<Books />
+							) : (
+								<Login
+									user={user}
+									setUser={setUser}
+									successMessage={successMessage}
+								/>
+							)
+						}
+					/>
+					<Route
+						path="/signup"
+						element={
+							<Signup setSuccessMessage={setSuccessMessage} />
+						}
+					/>
+					<Route path="/books/add" element={<BooksForm />} />
+					<Route
+						path="/books/:id"
+						element={<BookDetails user={user} />}
+					/>
+					<Route path="/books/:id/new" element={<PostsForm />} />
+					<Route
+						path="/posts/:id/update"
+						element={<PostsUpdateForm />}
+					/>
+					<Route
+						path="/favorites"
+						element={<Favorites user={user} />}
+					/>
+				</Routes>
+			</ScrollToTop>
 		</div>
 	)
 }
