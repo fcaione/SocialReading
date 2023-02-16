b5547ba32ac7853415f6faea4cae2b87b5b8aa70
Freddy Caione <fac299@nyu.edu>
2023-02-16 10:34:35 -0500

    sign out button and navbar stying

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,54 @@
 import { NavLink } from "react-router-dom"
+import icon from "../images/astronomy.png"
+import swal from "sweetalert"
 import "./NavBar.css"
 
 const NavBar = () => {
+
+	const signOut = () => {
+		localStorage.removeItem("userId")
+		window.location.reload()
+	}
+
+	const handleSignOut = async () => {
+		const response = await swal({
+			title: "Are you sure you want to sign out",
+			type: "warning",
+			buttons: true,
+			dangerMode: true,
+		})
+
+		if (response) signOut()
+	}
+
+
 	return (
 		<nav className="navbar">
 			<NavLink to="/">
-				<h3>Home</h3>
+				<div className="home-page">
+					<h2>LITERARY LOUNGE</h2>
+				</div>
 			</NavLink>
-			<NavLink to="/favorites">
-				<h3>Favorites</h3>
-			</NavLink>
+			<div className="flex-row">
+				<NavLink to="/favorites">
+					<div className="favorites-page">
+					<h3>Favorites</h3>
+					</div>
+				</NavLink>
+				<NavLink to="/about">
+					<div className="about-page">
+					<h3>About</h3>
+					</div>
+				</NavLink>
+				<div className="sign-out">
+				<button 
+				className="sign-out-btn"
+				onClick={handleSignOut}
+				>
+					<h3>Sign Out</h3>
+				</button>
+				</div>
+			</div>
 		</nav>
 	)
 }
